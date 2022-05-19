f15fa14c4e776e802a45efa1c3758fbde7e54ef0
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2022-05-19 20:55:07 -0300

    Change the name and implemented product class

---
+++ src/main/java/br/com/domain/Product.java
@@ -0,0 +1,42 @@
+package br.com.domain;
+
+import java.util.UUID;
+
+public class Product {
+
+    private UUID id;
+    private String name;
+    private double price;
+    private double amount;
+
+    public Product(){
+
+    }
+
+    public Product(UUID id, String name, double price, double amount){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public UUID getId() {return id;}
+
+    public String getName() {return name;}
+
+    public double getPrice() {return price;}
+
+    public double getAmount() {return amount;}
+
+    public Product(ProductDTO productDTO){
+        this.name = productDTO.getName();
+        this.price = productDTO.getPrice();
+        this.amount = productDTO.getAmount();
+    }
+
+    public void UpdateStock(double amount){ this.amount = amount;}
+
+    public ProductDTO toProductDTO(){
+        return new ProductDTO( this.name,  this.price,  this.amount);
+    }
+}
